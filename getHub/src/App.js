76be2942b919ac1a93ec09f7471052781d465302
avocad06 <miggo2704@gmail.com>
2023-04-21 16:36:49 +0900

    fix: queryClient 추가

--- getHub/src/App.js
+++ getHub/src/App.js
@@ -1,16 +1,27 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 
 import Home from './pages/Home';
 import User from './pages/User';
 
+const queryClient = new QueryClient()
+
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route element={<Home />} path='/'></Route>
-        <Route element={<User />} path='/users/:id'></Route>
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route element={<Home />} path='/'></Route>
+          <Route element={<User />} path='/users/:id'></Route>
+        </Routes>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </div >
 
 
